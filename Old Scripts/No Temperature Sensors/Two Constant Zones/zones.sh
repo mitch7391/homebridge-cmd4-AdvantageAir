4eccac77e295433995f8329d59575f487bba37f5
Mitch Williams <40288237+mitch7391@users.noreply.github.com>
2021-08-15 03:19:08 +0800

    Old zones.sh Script
    There is not a new version of this script yet. This will be added to the To-Do list.

---
+++ Old Scripts/No Temperature Sensors/Two Constant Zones/zones.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# IP address
+ip="192.168.0.173:2025"
+
+if [ "$1" = "Get" ]; then
+  case "$3" in
+    # Get zone open/closed status
+    On )
+      if [ "$(curl -s http://$ip/getSystemData | jq '.aircons.ac1.zones.'"$4"'.state')" = '"open"' ]; then
+        echo 1
+      else
+        echo 0
+      fi
+    ;;
+  esac
+fi
+
+if [ "$1" = "Set" ]; then
+  case "$3" in
+    # Set zone to open/close.
+    On )
+      if [ "$4" = "1" ]; then
+        curl -g http://$ip/setAircon?json={"ac1":{"zones":{"$5":{"state":"open"}}}}
+      else
+        curl -g http://$ip/setAircon?json={"ac1":{"zones":{"$5":{"state":"close"}}}}
+      fi
+    ;;
+  esac
+fi
+
+exit 0
