e9efdc1f883ec207db4441da76c2f54a6b9a9973
Ung Sing Wong <96530237+uswong@users.noreply.github.com>
2023-07-25 21:44:29 +0800

    Add success/fail messages

--- test/StartServer.sh
+++ test/StartServer.sh
@@ -17,16 +17,20 @@ node ./AirConServer.js >> "${TMPDIR}/AA-001/AirConServer.out" 2>&1 &
 rc=$?
 if [ "$rc" != 0 ]; then
    echo "Setup: Starting Daemon failed rc: $rc" >> "${TMPDIR}"/AA-001/AirConServer.out
+   echo "Starting AirConServer Daemon failed rc: $rc" >> "${TMPDIR}"/AA-001/AirConServer.out
 fi
 sleep 3
 echo "Setup: Daemon shold be started rc: $rc" >> "${TMPDIR}"/AA-001/AirConServer.out
+echo "AirConServer Daemon shold be started rc: $rc"
 
 # load the myAirData for MyPlace_Simulated system
 curl -s -g "http://localhost:2025/reInit"
-echo "myAirData=$1"
 curl -s -g "http://localhost:2025?load=testData/$1"
 if [ "$rc" != 0 ]; then
    echo "Data Loading: failed rc: $rc" >> "${TMPDIR}"/AA-001/AirConServer.out
+   echo "Data Loading: failed rc: $rc"
 else
    echo "Data Loading sucessful" >> "${TMPDIR}"/AA-001/AirConServer.out
+   echo "Data Loading sucessful"
+   echo "myAirData=$1"
 fi
--- test/StopServer.sh
+++ test/StopServer.sh
@@ -7,6 +7,8 @@ curl -s -g "http://localhost:2025/quit" >> "${TMPDIR}"/AA-001/AirConServer.out 2
 rc=$?
 if [ "$rc" != 0 ]; then
    echo "Setup: Stopping failed rc: $rc" >> "${TMPDIR}"/AA-001/AirConServer.out
+   echo "Stopping of AirConServer failed rc: $rc"
 else
    echo "Setup: Curl has Stoped daemon rc: $rc" >> "${TMPDIR}"/AA-001/AirConServer.out
+   echo "Curl has Stoped AirConServer daemon successfully rc: $rc"
 fi
